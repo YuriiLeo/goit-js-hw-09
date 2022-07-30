77664d5fc6b29614a060d7614ec05f1a24822978
YuriiLeo <yuriileontenko@gmail.com>
2022-07-31 01:59:06 +0300

    додав змінні і функції в ДЗ1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,42 @@
+const refs = {
+ btnStartChangeColor: document.querySelector("[data-start]"),
+ btnStopChangeColor: document.querySelector("[data-stop]")
+};
+const PROMPT_DELAY = 1000;
+let btnIsActive = false;
+// vuznachennya dovilnogo kolory
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, 0)}`;
+};
+
+// Povisuv zminy bcg 
+  function handleChangeColor() {
+    let color = getRandomHexColor();
+    console.log(color);
+    document.body.setAttribute('style', `background: ${color}`);
+};
+
+// Zmina bcg z intervalom
+function StartTimerChangeColor() {
+  if (btnIsActive) {
+    // refs.btnStartChangeColor.removeEventListener("click", StartTimerChangeColor); 
+    return;
+  }
+  timerId = setInterval(handleChangeColor, PROMPT_DELAY);
+  btnIsActive = true;
+}
+
+// Zupunka zminu
+function StopTimerChangeColor () {
+  clearInterval(timerId);
+  // refs.btnStartChangeColor.addEventListener("click", StartTimerChangeColor);
+  btnIsActive = false;
+}
+ 
+
+  
+refs.btnStartChangeColor.addEventListener("click", StartTimerChangeColor);
+refs.btnStopChangeColor.addEventListener("click", StopTimerChangeColor);
 
